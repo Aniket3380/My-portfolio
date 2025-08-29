c1be5abf1b83e642d288ad1201a8de8ff2c0f7f7
Aniket3380 <aniketdhanawade1011@gmail.com>
2025-08-29 21:15:43 +0530

    "in mywork added new project"

--- src/Component/Mywork.js
+++ src/Component/Mywork.js
@@ -5,20 +5,31 @@ import { Link } from "react-router-dom";
 export default function MyWork() {
   const projects = [
     {
-      title: "E-Commerce Website",
+      title: "E-Commerce Dashboard",
       description: "Built with MERN stack, features product listings, cart, and checkout.",
-      link: "https://github.com/Aniket3380/E-commerse-Dashborad.git"
+      link: "https://github.com/Aniket3380/E-commerse-Dashborad.git",
+      liveLink: "https://e-commerse-dashborad.vercel.app/signup",
     },
+
+    {
+    title: "Weather Dashboard",
+    description: "Weather app using React and OpenWeather API to get real-time weather data.",
+    link: "https://github.com/Aniket3380/Weather-Dashboard.git",
+    liveLink: "https://weather-dashboard-virid-six.vercel.app/"
+  },
     {
       title: "Portfolio Website",
       description: "Responsive portfolio made with React.js and CSS animations.",
-      link: "https://github.com/Aniket3380/My-portfolio"
+      link: "https://github.com/Aniket3380/My-portfolio",
+      liveLink: "https://my-portfolio-seven-sandy-11.vercel.app/",
     },
     {
       title: "Todo App",
       description: "Simple CRUD todo app using React and LocalStorage.",
-      link: "https://github.com/Aniket3380/My-todo-app"
-    }
+      link:"https://github.com/Aniket3380/My-todo-app.git",
+      liveLink: "https://my-todo-app-mu-self.vercel.app/"
+    },
+     
   ];
 
   return (
@@ -31,6 +42,9 @@ export default function MyWork() {
             <p>{project.description}</p>
             <Link to={project.link} className="work-btn" target="_blank" rel="noopener noreferrer">
               View Project
+            </Link>
+            <Link to={project.liveLink} className="work-btn live-link" target="_blank" rel="noopener noreferrer">
+              View Live Project
             </Link>
           </div>
         ))}
